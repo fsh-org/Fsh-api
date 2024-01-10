a5acf91e424d9e5440a93a9a1e3b368a96371c32
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-01-10 16:36:29 +0000

    Fix crash for videos with warning

--- apis/audio.js
+++ apis/audio.js
@@ -11,23 +11,39 @@ module.exports = {
   async execute(req, res) {
     let id = req.query['id'];
 
+    if (!ytdl.validateID(id)) {
+      res.json({
+        err: true,
+        msg: 'Invalid id'
+      })
+      return;
+    }
+
     let downloadOptions = {
       quality: 'highest',
       filter: 'audio'
     };
-    let videoUrl = 'https://www.youtube.com/watch?v='+id
-    ytdl(videoUrl, downloadOptions)
-      .pipe(fs.createWriteStream(`images/audio/${id}.mp3`))
-      .on('finish', () => {
-        res.json({
-          audio: `https://api.fsh.plus/images/audio/${id}.mp3`
+    let videoUrl = 'https://www.youtube.com/watch?v='+id;
+    try {
+      let info = await ytdl.getInfo(videoUrl)
+      ytdl.downloadFromInfo(info, downloadOptions)
+        .pipe(fs.createWriteStream(`images/audio/${id}.mp3`))
+        .on('finish', () => {
+          res.json({
+            audio: `https://api.fsh.plus/images/audio/${id}.mp3`
+          })
         })
+        .on('error', (error) => {
+          res.json({
+            err: true,
+            msg: 'Could not download'
+          })
+        });
+    } catch (err) {
+      res.json({
+        err: true,
+        msg: 'Could not download'
       })
-      .on('error', (error) => {
-        res.json({
-          err: true,
-          msg: 'Could not download'
-        })
-      });
+    }
   }
 }
