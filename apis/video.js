f5e4e492e240b4bfe48c82c7e54c030b4d041959
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-01-10 16:36:29 +0000

    Fix crash for videos with warning

--- apis/video.js
+++ apis/video.js
@@ -7,27 +7,43 @@ module.exports = {
   type: 'get',
   params: ["id", true],
   category: "image",
-  
+
   async execute(req, res) {
     let id = req.query['id'];
+
+    if (!ytdl.validateID(id)) {
+      res.json({
+        err: true,
+        msg: 'Invalid id'
+      })
+      return;
+    }
 
     let downloadOptions = {
       quality: 'highest',
       filter: 'audioandvideo'
     };
-    let videoUrl = 'https://www.youtube.com/watch?v='+id
-    ytdl(videoUrl, downloadOptions)
-      .pipe(fs.createWriteStream(`images/video/${id}.mp4`))
-      .on('finish', () => {
-        res.json({
-          video: `https://api.fsh.plus/images/video/${id}.mp4`
+    let videoUrl = 'https://www.youtube.com/watch?v='+id;
+    try {
+      let info = await ytdl.getInfo(videoUrl)
+      ytdl.downloadFromInfo(info, downloadOptions)
+        .pipe(fs.createWriteStream(`images/video/${id}.mp4`))
+        .on('finish', () => {
+          res.json({
+            video: `https://api.fsh.plus/images/video/${id}.mp4`
+          })
         })
+        .on('error', (error) => {
+          res.json({
+            err: true,
+            msg: 'Could not download'
+          })
+        });
+    } catch (err) {
+      res.json({
+        err: true,
+        msg: 'Could not download'
       })
-      .on('error', (error) => {
-        res.json({
-          err: true,
-          msg: 'Could not download'
-        })
-      });
+    }
   }
 }
