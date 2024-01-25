e5f979ba7d329469b233a62dcc93f0a5b4d4d520
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-01-25 15:38:59 +0000

    Math

---
+++ apis/math.js
@@ -0,0 +1,23 @@
+const Mexp = require("math-expression-evaluator");
+
+module.exports = {
+  path: '/math',
+  info: 'Evaluate a math expression',
+  type: 'get',
+  params: ["line", true],
+  category: "text",
+  async execute(req, res) {
+    const mexp = new Mexp()
+
+    let rs;
+    try {
+      rs = String(mexp.eval(req.url.split('line=')[1].split('&')[0].replaceAll('%20','')));
+    } catch (err) {
+      rs = 'invalid expression'
+    }
+
+    res.json({
+      result: rs
+    })
+  }
+}
