3e41b2ce6b14e76f88e41aad8d4b966edb48aaa2
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-03-02 19:28:11 +0000

    Uwuify & Periodic Table added + Requests 2

---
+++ apis/periodic-table.js
@@ -0,0 +1,37 @@
+const data = require('../text/pt.js')
+
+module.exports = {
+  path: '/periodic-table',
+  info: 'Get info on a element (search via symbol or number)',
+  type: 'get',
+  params: ['element', false],
+  category: "text",
+  
+  async execute(req, res) {
+    if (!req.query['element']) {
+      res.json(data[Math.ceil(Math.random()*data.length-1)])
+    } else {
+      if (Number(req.query['element'])) {
+        let hg = data[Number(req.query['element'])];
+        if (!hg) {
+          res.json({
+            err: true,
+            msg: 'Elemenet not found'
+          })
+          return;
+        }
+        res.json(hg)
+      } else {
+        let hg = data.filter(t => {return String(t.symbol).toLowerCase() == req.query['element'].toLowerCase()})[0];
+        if (!hg) {
+          res.json({
+            err: true,
+            msg: 'Elemenet not found'
+          })
+          return;
+        }
+        res.json(hg)
+      }
+    }
+  }
+}
