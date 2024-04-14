747cbdb5ed8624c9c6c31d57cda368ff5a2e0983
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-14 18:14:57 +0000

    Add http | Make requests better

---
+++ apis/http.js
@@ -0,0 +1,26 @@
+let codes = require('../text/httpcodes.js')
+
+module.exports = {
+  path: '/http',
+  info: 'Get info on a http code',
+  type: 'get',
+  params: ['code', false],
+  category: "text",
+  
+  async execute(req, res) {
+    if (req.query['code']) {
+      let code = codes[String(req.query['code'])];
+      if (code?.length) {
+        res.json(code)
+      } else {
+        res.json({
+          err: true,
+          msg: "code not found"
+        })
+      }
+    } else {
+      let keys = Object.keys(codes);
+      res.json(codes[keys[keys.length * Math.random() << 0]])
+    }
+  }
+}
