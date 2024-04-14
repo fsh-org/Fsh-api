4807868095238e20746aff08d0d06816b31fe6d6
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-14 18:14:57 +0000

    Add http | Make requests better

---
+++ text/httpcodes.js
@@ -0,0 +1,657 @@
+module.exports = {
+  "100": {
+    "code": 100,
+    "title": "Continue",
+    "type": "Informational",
+    "standard": "Base"
+  },
+  "101": {
+    "code": 101,
+    "title": "Switching Protocols",
+    "type": "Informational",
+    "standard": "Base"
+  },
+  "102": {
+    "code": 102,
+    "title": "Processing",
+    "type": "Informational",
+    "standard": "RFC 2518"
+  },
+  "103": {
+    "code": 103,
+    "title": "Early Hints",
+    "type": "Informational",
+    "standard": "RFC 8297"
+  },
+
+  "200": {
+    "code": 200,
+    "title": "Ok",
+    "type": "Success",
+    "standard": "Base"
+  },
+  "201": {
+    "code": 201,
+    "title": "Created",
+    "type": "Success",
+    "standard": "Base"
+  },
+  "202": {
+    "code": 202,
+    "title": "Accepted",
+    "type": "Success",
+    "standard": "Base"
+  },
+  "203": {
+    "code": 203,
+    "title": "Non-Authoritative Informational",
+    "type": "Success",
+    "standard": "HTTP/1.1"
+  },
+  "204": {
+    "code": 204,
+    "title": "No Content",
+    "type": "Success",
+    "standard": "Base"
+  },
+  "205": {
+    "code": 205,
+    "title": "Reset Content",
+    "type": "Success",
+    "standard": "Base"
+  },
+  "206": {
+    "code": 206,
+    "title": "Partial Content",
+    "type": "Success",
+    "standard": "Base"
+  },
+  "207": {
+    "code": 207,
+    "title": "Multi-Status",
+    "type": "Success",
+    "standard": "RFC 4918"
+  },
+  "208": {
+    "code": 208,
+    "title": "Already Reported",
+    "type": "Success",
+    "standard": "RFC 5842"
+  },
+  "226": {
+    "code": 226,
+    "title": "IM Used",
+    "type": "Success",
+    "standard": "RFC 3229"
+  },
+
+  "300": {
+    "code": 300,
+    "title": "Multiple Choices",
+    "type": "Redirection",
+    "standard": "Base"
+  },
+  "301": {
+    "code": 301,
+    "title": "Moved Permanently",
+    "type": "Redirection",
+    "standard": "Base"
+  },
+  "302": {
+    "code": 302,
+    "title": "Found",
+    "type": "Redirection",
+    "standard": "Base"
+  },
+  "303": {
+    "code": 303,
+    "title": "See Other",
+    "type": "Redirection",
+    "standard": "HTTP/1.1"
+  },
+  "304": {
+    "code": 304,
+    "title": "Not Modified",
+    "type": "Redirection",
+    "standard": "Base"
+  },
+  "305": {
+    "code": 305,
+    "title": "Use Proxy",
+    "type": "Redirection",
+    "standard": "HTTP/1.1"
+  },
+  "306": {
+    "code": 306,
+    "title": "Switch Proxy",
+    "type": "Redirection",
+    "standard": "Base"
+  },
+  "307": {
+    "code": 307,
+    "title": "Temporary Redirect",
+    "type": "Redirection",
+    "standard": "HTTP/1.1"
+  },
+  "308": {
+    "code": 308,
+    "title": "Permanent Redirect",
+    "type": "Redirection",
+    "standard": "Base"
+  },
+
+  "400": {
+    "code": 400,
+    "title": "Bad Request",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "401": {
+    "code": 401,
+    "title": "Unauthorized",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "402": {
+    "code": 402,
+    "title": "Payment Required",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "403": {
+    "code": 403,
+    "title": "Forbidden",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "404": {
+    "code": 404,
+    "title": "Not Found",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "405": {
+    "code": 405,
+    "title": "Method Not Allowed",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "406": {
+    "code": 406,
+    "title": "Not Acceptable",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "407": {
+    "code": 407,
+    "title": "Proxy Authentication Required",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "408": {
+    "code": 408,
+    "title": "Request Timeout",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "409": {
+    "code": 409,
+    "title": "Conflict",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "410": {
+    "code": 410,
+    "title": "Gone",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "411": {
+    "code": 411,
+    "title": "Length Required",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "412": {
+    "code": 412,
+    "title": "Precondition Failed",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "413": {
+    "code": 413,
+    "title": "Payload Too Large",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "414": {
+    "code": 414,
+    "title": "URI Too Long",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "415": {
+    "code": 415,
+    "title": "Unsupported Media Type",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "416": {
+    "code": 416,
+    "title": "Range Not Satisfiable",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "417": {
+    "code": 417,
+    "title": "Expectation Failed",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "418": {
+    "code": 418,
+    "title": "I'm a teapot",
+    "type": "Client Error",
+    "standard": "RFC 2324, RFC 7168, Hyper Text Coffee Pot Control Protocol"
+  },
+  "421": {
+    "code": 421,
+    "title": "Misdirected Request",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "422": {
+    "code": 422,
+    "title": "Unprocessable Content",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "423": {
+    "code": 423,
+    "title": "Locked",
+    "type": "Client Error",
+    "standard": "RFC 4918"
+  },
+  "424": {
+    "code": 424,
+    "title": "Failed Dependency",
+    "type": "Client Error",
+    "standard": "RFC 4918"
+  },
+  "425": {
+    "code": 425,
+    "title": "Too Early",
+    "type": "Client Error",
+    "standard": "RFC 8470"
+  },
+  "426": {
+    "code": 426,
+    "title": "Upgrade Required",
+    "type": "Client Error",
+    "standard": "Base"
+  },
+  "428": {
+    "code": 428,
+    "title": "Precondition Required",
+    "type": "Client Error",
+    "standard": "RFC 6585"
+  },
+  "429": {
+    "code": 429,
+    "title": "Too Many Requests",
+    "type": "Client Error",
+    "standard": "RFC 6585"
+  },
+  "431": {
+    "code": 431,
+    "title": "Request Header Fields Too Large",
+    "type": "Client Error",
+    "standard": "RFC 6585"
+  },
+  "451": {
+    "code": 451,
+    "title": "Unavailable For Legal Reasons",
+    "type": "Client Error",
+    "standard": "RFC 7725"
+  },
+
+  "500": {
+    "code": 500,
+    "title": "Internal Server Error",
+    "type": "Server Error",
+    "standard": "Base"
+  },
+  "501": {
+    "code": 501,
+    "title": "Not Implemented",
+    "type": "Server Error",
+    "standard": "Base"
+  },
+  "502": {
+    "code": 502,
+    "title": "Bad Gateway",
+    "type": "Server Error",
+    "standard": "Base"
+  },
+  "503": {
+    "code": 503,
+    "title": "Service Unavailable",
+    "type": "Server Error",
+    "standard": "Base"
+  },
+  "504": {
+    "code": 504,
+    "title": "Gateway Timeout",
+    "type": "Server Error",
+    "standard": "Base"
+  },
+  "505": {
+    "code": 505,
+    "title": "HTTP Version Not Supported",
+    "type": "Server Error",
+    "standard": "Base"
+  },
+  "506": {
+    "code": 506,
+    "title": "Variant Also Negotiates",
+    "type": "Server Error",
+    "standard": "RFC 2295"
+  },
+  "507": {
+    "code": 507,
+    "title": "Insufficient Storage",
+    "type": "Server Error",
+    "standard": "RFC 4918"
+  },
+  "508": {
+    "code": 508,
+    "title": "Loop Detected",
+    "type": "Server Error",
+    "standard": "RFC 5842"
+  },
+  "509": {
+    "code": 509,
+    "title": "Starting Ticket Required",
+    "type": "Server Error",
+    "standard": "RFC 5842"
+  },
+  "510": {
+    "code": 510,
+    "title": "Not Extended",
+    "type": "Server Error",
+    "standard": "RFC 2774"
+  },
+  "511": {
+    "code": 511,
+    "title": "Network Authentication Required",
+    "type": "Server Error",
+    "standard": "RFC 6585"
+  },
+
+  "604": {
+    "code": 604,
+    "title": "Inaccessible Request",
+    "type": "Multi-sided Errors",
+    "standard": "RFC 818"
+  },
+  "605": {
+    "code": 605,
+    "title": "Not Allowed",
+    "type": "Multi-sided Errors",
+    "standard": "RFC 4271"
+  },
+  "644": {
+    "code": 644,
+    "title": "Request Deleted or Modified",
+    "type": "Multi-sided Errors",
+    "standard": "RFC 6778"
+  },
+
+  "218": {
+    "code": 218,
+    "title": "This is fine",
+    "type": "Unofficial",
+    "standard": "Apache server"
+  },
+  "419": {
+    "code": 419,
+    "title": "Page Expired",
+    "type": "Unofficial",
+    "standard": "Laravel Framework"
+  },
+  "420": {
+    "code": 420,
+    "title": "Method Failure | Enhance Your Calm",
+    "type": "Unofficial",
+    "standard": "Spring Framework | Twitter"
+  },
+  "430": {
+    "code": 430,
+    "title": "Shopify Security Rejection | Request Header Fields Too Large |",
+    "type": "Unofficial",
+    "standard": "Shopify"
+  },
+  "450": {
+    "code": 450,
+    "title": "Blocked by Windows Parental Controls",
+    "type": "Unofficial",
+    "standard": "Microsoft"
+  },
+  "498": {
+    "code": 498,
+    "title": "Invalid Token",
+    "type": "Unofficial",
+    "standard": "Esri"
+  },
+  "499": {
+    "code": 499,
+    "title": "499 Token Required",
+    "type": "Unofficial",
+    "standard": "Esri"
+  },
+  "509": {
+    "code": 509,
+    "title": "Bandwidth Limit Exceeded",
+    "type": "Unofficial",
+    "standard": "Apache Web Server, cPanel"
+  },
+  "529": {
+    "code": 529,
+    "title": "Site is overloaded",
+    "type": "Unofficial",
+    "standard": "Qualys, SSLLabs"
+  },
+  "530": {
+    "code": 530,
+    "title": "Site is frozen | Origin DNS Error",
+    "type": "Unofficial",
+    "standard": "Pantheon Systems | Shopify"
+  },
+  "540": {
+    "code": 540,
+    "title": "Temporarily Disabled",
+    "type": "Unofficial",
+    "standard": "Shopify"
+  },
+  "589": {
+    "code": 589,
+    "title": "Network read timeout error",
+    "type": "Unofficial",
+    "standard": "Base"
+  },
+  "599": {
+    "code": 599,
+    "title": "Network Connect Timeout Error",
+    "type": "Unofficial",
+    "standard": "Base"
+  },
+  "770": {
+    "code": 770,
+    "title": "Game's root place is not active",
+    "type": "Unofficial",
+    "standard": "Fortnite, Roblox, Zepeto"
+  },
+  "783": {
+    "code": 783,
+    "title": "Unexpected Token",
+    "type": "Unofficial",
+    "standard": "Shopify"
+  },
+  "611": {
+    "code": 611,
+    "title": "Unexpected Unix Setpoint",
+    "type": "Unofficial",
+    "standard": "CloudFront, Shopify"
+  },
+  "440": {
+    "code": 440,
+    "title": "Login Time-out",
+    "type": "Unofficial",
+    "standard": "Internet Information Services"
+  },
+  "449": {
+    "code": 449,
+    "title": "Retry With",
+    "type": "Unofficial",
+    "standard": "Internet Information Services"
+  },
+  "451": {
+    "code": 451,
+    "title": "Redirect",
+    "type": "Unofficial",
+    "standard": "Internet Information Services"
+  },
+  "520": {
+    "code": 520,
+    "title": "Web Server Returned an Unknown Error",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "521": {
+    "code": 521,
+    "title": "Web Server Is Down",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "522": {
+    "code": 522,
+    "title": "Connection Timed Out",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "523": {
+    "code": 523,
+    "title": "Origin Is Unreachable",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "524": {
+    "code": 524,
+    "title": "A Timeout Occurred",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "525": {
+    "code": 525,
+    "title": "SSL Handshake Failed",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "526": {
+    "code": 526,
+    "title": "Invalid SSL Certificate",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "527": {
+    "code": 527,
+    "title": "Railgun Error (obsolete)",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "530": {
+    "code": 530,
+    "title": "1xxx Error",
+    "type": "Unofficial",
+    "standard": "Cloudflare"
+  },
+  "000": {
+    "code": 000,
+    "title": "Headers Too Big or Too Many Requests",
+    "type": "Unofficial",
+    "standard": "AWS Elastic Load Balancing"
+  },
+  "460": {
+    "code": 460,
+    "title": "Client Closed Connection Before Idle",
+    "type": "Unofficial",
+    "standard": "AWS Elastic Load Balancing"
+  },
+  "463": {
+    "code": 463,
+    "title": "Too Many Forward Ips",
+    "type": "Unofficial",
+    "standard": "AWS Elastic Load Balancing"
+  },
+  "464": {
+    "code": 464,
+    "title": "Incompatible Protocol Version",
+    "type": "Unofficial",
+    "standard": "AWS Elastic Load Balancing"
+  },
+  "561": {
+    "code": 561,
+    "title": "Unauthorized",
+    "type": "Unofficial",
+    "standard": "AWS Elastic Load Balancing"
+  },
+  "911": {
+    "code": 911,
+    "title": "A bug has hit the server tower",
+    "type": "Unofficial",
+    "standard": ".gov"
+  },
+  
+  "110": {
+    "code": 110,
+    "title": "Response is Stale",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  },
+  "111": {
+    "code": 111,
+    "title": "Revalidation Failed",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  },
+  "112": {
+    "code": 112,
+    "title": "Disconnected Operation",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  },
+  "113": {
+    "code": 113,
+    "title": "Heuristic Expiration",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  },
+  "199": {
+    "code": 199,
+    "title": "Miscellaneous Warning",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  },
+  "214": {
+    "code": 214,
+    "title": "Transformation Applied",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  },
+  "299": {
+    "code": 299,
+    "title": "Miscellaneous Persistent Warning",
+    "type": "Caching warning codes (obsoleted)",
+    "standard": "RFC 7234"
+  }
+}
