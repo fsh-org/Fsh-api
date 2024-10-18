54eeb2efed613465d4e675d45786ba845d8e5d7e
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-18 18:10:02 +0000

    Image format

---
+++ apis/image-format.js
@@ -0,0 +1,44 @@
+const sharp = require('sharp');
+const rewrites = {
+  jpg: 'jpeg',
+  jfif: 'jpeg',
+  'jpg-large': 'jpeg',
+  apng: 'png'
+};
+const rewritesKeys = Object.keys(rewrites);
+
+module.exports = {
+  path: '/image-format',
+  info: 'Change the format of a image',
+  type: 'post',
+  params: [],
+  category: "image",
+  
+  async execute(req, res) {
+    if (!req.body || !req.body.length) {
+      res.error('You must pass a image in the request body')
+      return;
+    }
+    if (!req.query['format']) {
+      res.error('Include a format')
+      return;
+    }
+    let format = req.query['format'];
+    format = format.toLowerCase().trim();
+    if (rewritesKeys.includes(format)) {
+      format = rewrites[format];
+    }
+    sharp(req.body)
+      .toFormat(format)
+      .toBuffer()
+      .then(outputBuffer => {
+        res.json({
+          image: 'data:image/'+format+';base64,' + outputBuffer.toString('base64')
+        })
+      })
+      .catch(err => {
+        res.error('Could not generate')
+        return;
+      })
+  }
+}
