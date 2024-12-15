6550852396b7878c44e89f6d44d3dd14d0aa10fb
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-12-15 17:31:15 +0100

    The final endpoints

--- apis/generate.js
+++ apis/generate.js
@@ -24,13 +24,13 @@ module.exports = {
   async execute(req, res) {
     try {
       if(!req.query["text"]) {
-        res.send('mini docs<br>text=[TEXT] - Text for generation<br>Optional:<br>model=[Model] - Very high level but allows for better text<br>when generating text a object will be automaticly created with all the past conversation (if not set every call will be treated as a new conversation)');
+        res.type('text/plain').send('mini docs<br>text=[TEXT] - Text for generation<br>Optional:<br>model=[Model] - Very high level but allows for better text<br>when generating text a object will be automaticly created with all the past conversation (if not set every call will be treated as a new conversation)');
         return;
       };
       let con;
       if (req.query["conversation"]) {
         try {
-          con = JSON.parse(req.query["conversation"])
+          con = JSON.parse(req.query["conversation"]);
         } catch (err) {
           con = {
             past_user_inputs: [],
@@ -44,30 +44,27 @@ module.exports = {
         };
       }
       let txt = req.query["text"];
-      const response = await fetch(
-        "https://api-inference.huggingface.co/models/"+(req.query['model'] || 'google/gemma-2b-it'),
-        {
-          headers: {
-            Authorization: `Bearer ${process.env['ai']}`,
-            'Content-Type': 'application/json'
-          },
-          method: "POST",
-          body: JSON.stringify({
-            inputs: txt,
-            past_user_inputs: con.past_user_inputs || [],
-            generated_responses: con.generated_responses || []
-          })
-        }
-      );
+      const response = await fetch(`https://api-inference.huggingface.co/models/${req.query['model'] ?? 'google/gemma-2b-it'}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.ai}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          inputs: txt,
+          past_user_inputs: con.past_user_inputs ?? [],
+          generated_responses: con.generated_responses ?? []
+        })
+      });
       let result = await response.json();
       if (result.error) {
         throw new Error(result.error)
       }
 
-      let pu = (con.past_user_inputs || []);
-      pu.push(txt)
-      let pb = (con.generated_responses || []);
-      pb.push(result[0].generated_text)
+      let pu = (con.past_user_inputs ?? []);
+      pu.push(txt);
+      let pb = (con.generated_responses ?? []);
+      pb.push(result[0].generated_text);
 
       res.json({
         generated_text: result[0].generated_text,
@@ -77,7 +74,7 @@ module.exports = {
         }
       })
     } catch (err) {
-      res.error(err, 500)
+      res.error(err, 500);
     }
   }
 }
