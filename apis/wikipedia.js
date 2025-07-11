b15fe533b0ca2babf2b99764b842efed1a8954e9
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-07-11 13:20:52 +0200

    Better wikipedia

--- apis/wikipedia.js
+++ apis/wikipedia.js
@@ -16,6 +16,11 @@ module.exports = {
       name: 'html',
       required: false,
       default: 'false'
+    },
+    {
+      name: 'lang',
+      required: false,
+      default: 'en'
     }
   ],
   category: "text",
@@ -27,16 +32,30 @@ module.exports = {
     }
 
     let doc = await wtf.fetch(req.query['page'], {
-      lang: 'en',
+      lang: req.query['lang']??'en',
       'Api-User-Agent': `FshApi/1.0 (User, ${req.clientIp})`
-    })
+    });
+
+    if (doc===null) {
+      res.error('Page not found');
+      return;
+    }
 
     let data = '';
     if (req.query['html']?.toLowerCase()==='true') {
       data = doc.html({ infoboxes: false });
       data = data
         .replaceAll(/>[ |\t|\n]{2,}</g, '><') // Clean up whitespace
-        .replaceAll(/<a class="link" href="(.*?)">/g, function(_match, g1){return `<a class="link" href="${new URL(g1, doc.url())}">`}); // Link urls
+        // Link urls
+        .replaceAll(/<a class="link" href="(.*?)">/g, function(_match, g1){
+          let url = '';
+          try {
+            url = new URL(g1.replaceAll(/<[a-zA-Z0-9/]+?>/g,''), doc.url());
+          } catch(err) {
+            url = g1;
+          }
+          return `<a class="link" href="${url}">`;
+        });
     } else {
       data = doc.markdown({ infoboxes: false });
       // Link urls
@@ -47,8 +66,8 @@ module.exports = {
       title: doc.title(),
       lang: doc.lang(),
       description: doc.description(),
-      img: doc.images()[0].url(),
+      img: doc.images()[0]?.url(),
       data
-    })
+    });
   }
 }
