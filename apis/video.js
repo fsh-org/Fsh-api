c91975a2bde5d4cf1154ac1933955009c3925732
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-09-06 12:54:26 +0200

    Newgrounds support for Video&Audio

--- apis/video.js
+++ apis/video.js
@@ -1,92 +1,91 @@
-const fs = require('fs');
-const ytdl = require('@distube/ytdl-core');
+const fs = require('node:fs');
+const path = require('node:path');
 
+// Youtube
+const ytdl = require('@distube/ytdl-core');
+function isYT(id) {
+ return ytdl.validateID(id);
+}
+
+// Newgrounds
+let ngcache = new Map();
+function isNG(id) {
+  return (/^[0-9]+$/).test(id);
+}
+
+// Main
 module.exports = {
   path: '/video',
-  info: 'Download a youtube video into a mp4',
+  info: 'Download a video from: youtube, newgrounds',
   type: 'get',
   params: [
     {
       name: 'id',
       required: true,
       default: 'dQw4w9WgXcQ'
-    },
-    {
-      name: 'max',
-      required: false,
-      default: '25000000'
     }
   ],
   category: "image",
 
   async execute(req, res) {
     let id = req.query['id'];
-
-    if (!id || !ytdl.validateID(id)) {
-      res.error('Invalid id');
+    if (!id) {
+      res.error('Provide an id');
       return;
     }
 
-    if (fs.existsSync(`images/video/${id}.mp4`)) {
-      res.json({
-        video: `https://api.fsh.plus/images/video/${id}.mp4`,
-        download: `https://api.fsh.plus/download/video/${id}.mp4`
-      })
-      return;
-    }
+    id = id.toString().replaceAll(/[^a-zA-Z0-9_-]/g,'');
 
-    let downloadOptions = {
-      quality: 'highest',
-      filter: 'audioandvideo'
-    };
-    let videoUrl = 'https://www.youtube.com/watch?v='+id;
-    try {
-      let info = await ytdl.getInfo(videoUrl)
-      ytdl.downloadFromInfo(info, downloadOptions)
-        .pipe(fs.createWriteStream(`images/video/${id}.mp4`))
-        .on('finish', () => {
-          if (req.query['max']?.length) {
-            let stats = fs.statSync(`images/video/${id}.mp4`);
-            if (stats.size > (Number(req.query['max'])||25000000)) {
-              downloadOptions.quality = 'lowest';
-              ytdl.downloadFromInfo(info, downloadOptions)
-                .pipe(fs.createWriteStream(`images/video/${id}_low.mp4`))
-                .on('finish', () => {
-                  let stats2 = fs.statSync(`images/video/${id}_low.mp4`);
-                  if (stats2.size > (Number(req.query['max'])||25000000)) {
-                    res.json({
-                      err: true,
-                      msg: 'Video bigger than allowed, see full at https://api.fsh.plus/images/video/'+id+'.mp4',
-                      full: `https://api.fsh.plus/images/video/${id}.mp4`
-                    })
-                    return;
-                  }
-                  res.json({
-                    video: `https://api.fsh.plus/images/video/${id}_low.mp4`,
-                    download: `https://api.fsh.plus/download/video/${id}_low.mp4`,
-                    lower: true,
-                    full: `https://api.fsh.plus/images/video/${id}.mp4`
-                  })
-                  return;
-                })
-            } else {
-              res.json({
-                video: `https://api.fsh.plus/images/video/${id}.mp4`,
-                download: `https://api.fsh.plus/download/video/${id}.mp4`
-              })
-            }
-          } else {
+    if (isYT(id)) {
+      if (fs.existsSync(path.resolve('images/video', `${id}.mp4`))) {
+        res.json({
+          video: `https://api.fsh.plus/images/video/${id}.mp4`,
+          download: `https://api.fsh.plus/download/video/${id}.mp4`
+        });
+        return;
+      }
+      try {
+        let info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${id}`)
+        ytdl.downloadFromInfo(info, { quality: 'highest', filter: 'audioandvideo' })
+          .pipe(fs.createWriteStream(path.resolve('images/video', `${id}.mp4`)))
+          .on('finish', ()=>{
             res.json({
               video: `https://api.fsh.plus/images/video/${id}.mp4`,
               download: `https://api.fsh.plus/download/video/${id}.mp4`
-            })
-          }
+            });
+          })
+          .on('error', () => {
+            res.error('Could not download', 500);
+          });
+      } catch (err) {
+        res.error('Could not download', 500);
+      }
+    } else if (isNG(id)) {
+      if (ngcache.has(id)) {
+        res.json({
+          video: ngcache.get(id),
+          download: ngcache.get(id)
+        });
+        return;
+      }
+      fetch(`https://www.newgrounds.com/portal/video/${id}`, {
+        headers: {
+          'x-requested-with': 'XMLHttpRequest'
+        }
+      })
+        .then(ng=>ng.json())
+        .then(ng=>{
+          let highest = Object.keys(ng.sources).map(k=>k.replace('p','')).toSorted((a,b)=>b-a)[0]+'p';
+          if (ng.sources[highest].length>1) ng.sources[highest].sort((a,b)=>(b.type==='video/mp4')-(a.type==='video/mp4'));
+          ngcache.set(id, ng.sources[highest][0].src);
+          res.json({
+            video: ng.sources[highest][0].src,
+            download: ng.sources[highest][0].src
+          });
         })
-        .on('error', () => {
+        .catch(err=>{
           res.error('Could not download', 500);
         });
-    } catch (err) {
-      res.error('Could not download', 500);
     }
   }
 }
