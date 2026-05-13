e4a3ab3b153ab065b463cf81bd256dd64d086b50
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-05-13 21:15:26 +0200

    UUID 1 & 7 | Cleanup | Update deps

--- apis/sam.js
+++ apis/sam.js
@@ -2,28 +2,6 @@ const SamJs = require('sam-js');
 
 let sam = new SamJs();
 
-let text2Uint8Array = text => {
-  let buffer = new Uint8Array(text.length);
-  text.split('').forEach((e, index) => {
-    buffer[index] = e.charCodeAt(0);
-  });
-  return buffer;
-};
-let Uint32ToUint8Array = uint32 => {
-  let result = new Uint8Array(4);
-  result[0] = uint32;
-  result[1] = uint32 >> 8;
-  result[2] = uint32 >> 16;
-  result[3] = uint32 >> 24;
-  return result;
-};
-let Uint16ToUint8Array = uint16 => {
-  let result = new Uint8Array(2);
-  result[0] = uint16;
-  result[1] = uint16 >> 8;
-  return result;
-};
-
 module.exports = {
   path: '/sam',
   info: 'Make SAM say anything',
@@ -44,39 +22,10 @@ module.exports = {
       return;
     }
 
-    const audiobuffer = sam.buf8(text);
-
-    let realbuffer = new Uint8Array(44 + audiobuffer.length);
-    let pos = 0;
-
-    let write;
-    try {
-      write = buffer => {
-        realbuffer.set(buffer, pos);
-        pos += buffer.length;
-      };
-    } catch (err) {
-      res.error('Could not convert', 500);
-      return;
-    }
-
-    write(text2Uint8Array('RIFF'));
-    write(Uint32ToUint8Array(audiobuffer.length + 28));
-    write(text2Uint8Array('WAVE'));
-    write(text2Uint8Array('fmt '));
-    write(Uint32ToUint8Array(16));
-    write(Uint16ToUint8Array(1));
-    write(Uint16ToUint8Array(1));
-    write(Uint32ToUint8Array(22050));
-    write(Uint32ToUint8Array(22050));
-    write(Uint16ToUint8Array(1));
-    write(Uint16ToUint8Array(8));
-    write(text2Uint8Array('data'));
-    write(Uint32ToUint8Array(audiobuffer.length));
-    write(audiobuffer);
+    const audiobuffer = sam.wav(text);
 
     res.json({
-      audio: `data:audio/wav;base64,${Buffer.from(realbuffer).toString('base64')}`
-    })
+      audio: `data:audio/wav;base64,${Buffer.from(audiobuffer).toString('base64')}`
+    });
   }
 }
--- apis/uuid.js
+++ apis/uuid.js
@@ -1,8 +1,27 @@
 const crypto = require('node:crypto');
 
-let namespace;
-let name;
+const EPOCH_OFFSET = 12219292800000n;
 
+function v1UUID() {
+  let now = (BigInt(Date.now()) + EPOCH_OFFSET) * 10000n;
+
+  let timeLow = Number(now & 0xffffffffn);
+  let timeMid = Number((now >> 32n) & 0xffffn);
+  let timeHi = Number((now >> 48n) & 0x0fffn);
+
+  // Random
+  let clockSeq = crypto.randomBytes(2);
+  clockSeq[0] = (clockSeq[0] & 0x3f) | 0x80;
+  let node = crypto.randomBytes(6);
+
+  return [
+    timeLow.toString(16).padStart(8, '0'),
+    timeMid.toString(16).padStart(4, '0'),
+    (timeHi | 0x1000).toString(16).padStart(4, '0'),
+    clockSeq.toString('hex'),
+    node.toString('hex')
+  ].join('-');
+}
 function namespaceUUID(version, namespace, name) {
   // Parse the namespace UUID
   const namespaceBuffer = Buffer.from(namespace.replace(/-/g, ''), 'hex');
@@ -18,10 +37,8 @@ function namespaceUUID(version, namespace, name) {
 
   return `${uuidBytes.toString('hex', 0, 4)}-${uuidBytes.toString('hex', 4, 6)}-${uuidBytes.toString('hex', 6, 8)}-${uuidBytes.toString('hex', 8, 10)}-${uuidBytes.toString('hex', 10, 16)}`;
 }
-function generateVersion6UUID() {
+function v6UUID() {
   // UUID epoch offset
-  const EPOCH_OFFSET = 12219292800000n;
-
   let timestamp = (BigInt(Date.now()) + EPOCH_OFFSET) * 10000n;
 
   // Rearrange timestamp
@@ -30,16 +47,41 @@ function generateVersion6UUID() {
   let timeLow = Number(timestamp & 0xfffn);
 
   // Random data
-  let clockSeq = crypto.getRandomValues(new Uint16Array(1))[0] & 0x3fff;
-  let node = crypto.getRandomValues(new Uint8Array(6));
+  let clockSeq = crypto.randomBytes(2).readUInt16BE() & 0x3fff;
+  let node = crypto.randomBytes(6);
 
   return [
     timeHigh.toString(16).padStart(8, '0'),
     timeMid.toString(16).padStart(4, '0'),
     ((0x6000 | timeLow).toString(16)).padStart(4, '0'),
     ((0x8000 | clockSeq).toString(16)).padStart(4, '0'),
-    [...node].map(b=>b.toString(16).padStart(2, '0')).join('')
+    node.toString('hex')
   ].join('-');
+}
+function v7UUID() {
+  let bytes = crypto.randomBytes(16);
+
+  const now = BigInt(Date.now());
+
+  bytes[0] = Number((now >> 40n) & 0xffn);
+  bytes[1] = Number((now >> 32n) & 0xffn);
+  bytes[2] = Number((now >> 24n) & 0xffn);
+  bytes[3] = Number((now >> 16n) & 0xffn);
+  bytes[4] = Number((now >> 8n) & 0xffn);
+  bytes[5] = Number(now & 0xffn);
+
+  bytes[6] = (bytes[6] & 0x0f) | 0x70;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+
+  let hex = [...bytes].map(b=>b.toString(16).padStart(2, '0'));
+
+  return [
+    hex.slice(0, 6).join(''),
+    hex.slice(6, 8).join(''),
+    hex.slice(8, 10).join(''),
+    hex.slice(10, 12).join(''),
+    hex.slice(12, 16).join('')
+  ].join('-')
 }
 
 function upd(namespace) {
@@ -52,7 +94,7 @@ function upd(namespace) {
 
 module.exports = {
   path: '/uuid',
-  info: 'Generates random uuid (set version to help for list of versions)',
+  info: 'Generates uuids (set version to help for list of versions)',
   type: 'get',
   params: [
     {
@@ -82,7 +124,14 @@ module.exports = {
     }
     switch (req.query['version']) {
       case 'help':
-        res.send(`Version list (uses DCE 1.1, ISO/IEC 11578:1996 variant)<br><br>nil/null/0 - non unique uuid for testing<br>3 - Predictible uuid from name, requires "space" and "name" parameters, space must be exactly url, dns, oid, x500 or a valid uuid<br>4 - Random unique uuid<br>5 - Same as 3 but different hashing<br>6 - Similar to 1 but better sorting<br><br>More soon`)
+        res.type('text/html').send(`Version list (uses DCE 1.1, ISO/IEC 11578:1996 variant)<br><br>
+nil/null/0 - non unique uuid for testing<br>
+1 - Pseudo random timestamp generated uuid<br>
+3 - Predictible uuid from a name and namespace (space & name parameters), space can be url, dns, oid, x500 or a valid uuid<br>
+4 - Random uuid<br>
+5 - Same as 3 but different hashing<br>
+6 - Similar to 1 but better sorting<br>
+7 - Similar to 6 but unix timestamp`);
         return;
       case 'nil':
       case 'null':
@@ -91,10 +140,15 @@ module.exports = {
           uuid: '00000000-0000-0000-0000-000000000000'
         });
         return;
+      case '1':
+        res.json({
+          uuid: v1UUID()
+        });
+        return
       case '5':
       case '3':
-        namespace = upd(req.query['space']);
-        name = req.query['name'];
+        let namespace = upd(req.query['space']);
+        let name = req.query['name'];
         if (!namespace || !name) {
           res.error('Include a name and namespace');
           return;
@@ -110,7 +164,12 @@ module.exports = {
         return;
       case '6':
         res.json({
-          uuid: generateVersion6UUID()
+          uuid: v6UUID()
+        });
+        return
+      case '7':
+        res.json({
+          uuid: v7UUID()
         });
         return
     }
